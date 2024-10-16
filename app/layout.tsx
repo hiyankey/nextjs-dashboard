b3f45b41790d1723c762c6f171977299d729edf0
hiyankey <hiyankey@gmail.com>
2024-10-16 21:38:24 +0000

    chore:link project to db

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import '@/app/ui/global.css'
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>{children}</body>
     </html>
-  );
+  )
 }
